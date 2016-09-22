1f4041590a1bd11a973eee35afc8b826001de5b6
cike <875569847@qq.com>
2016-09-22 09:26:00 +0800

    整理格式

--- src/com/cike/response/DownloadServlet.java
+++ src/com/cike/response/DownloadServlet.java
@@ -19,45 +19,40 @@ import javax.servlet.http.HttpServletResponse;
 public class DownloadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//获取文件，读到流中
-		//获取文件的绝对磁盘路径
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 获取文件，读到流中
+		// 获取文件的绝对磁盘路径
 		String path = getServletContext().getRealPath("/img/游戏美女.jpg");
-		//截取文件名称
+		// 截取文件名称
 		String fileName = null;
 		int index = path.lastIndexOf("\\");
-		if (index!=-1) {
-			fileName = path.substring(index+1);
+		if (index != -1) {
+			fileName = path.substring(index + 1);
 		}
 		String agent = request.getHeader("User-Agent");
 		System.out.println(agent);
-		//判断浏览器
+		// 判断浏览器
 		if (agent.contains("MSIE")) {
-			fileName = URLEncoder.encode(fileName,"UTF-8");
+			fileName = URLEncoder.encode(fileName, "UTF-8");
 		}
-		//设置头信息
-		response.setHeader("Content-Disposition","attachment;filename="+fileName);
+		// 设置头信息
+		response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
 		System.out.println(path);
 		InputStream in = new FileInputStream(path);
-		//通过response读到客户端
+		// 通过response读到客户端
 		OutputStream os = response.getOutputStream();
-		//io的拷贝
+		// io的拷贝
 		byte[] b = new byte[1024];
 		int len = 0;
-		while((len = in.read(b))!=-1){
-			os.write(b,0,len);
+		while ((len = in.read(b)) != -1) {
+			os.write(b, 0, len);
 		}
 		in.close();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
